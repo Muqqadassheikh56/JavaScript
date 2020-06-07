69dc6be27282d3ae72fcb1b65725d9105937e631
muqqadassheikh56 <muqqadassheikh56@gmail.com>
2020-06-07 15:32:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// Chapter 1 - Alert
+
+// Task#01
+// alert("Hi, Welcome To our site!!!"); 
+
+// Task#02
+// alert("Error! Please enter vaild password.");
+
+// Task#03
+// alert("Welcome to JS Land...\nHappy Coding");
+
+// Task#04
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Task#05
+// alert("Hello.. I can run JS through web browser console")
+
+// Task#06
+// Task#07
+
+// Chapter 2 - Variables
+
+// Task#01
+var username;
+
+// Task#02
+var myName = "Jhone Doe";
+
+// Task#03
+// var message = "Hello World"
+// alert(message);
+
+// Task#04
+// var studentName = "Jhone";
+// var studentAge = "15 years old";
+// alert(studentName);
+// alert(studentAge);
+
+// Task#05
+// var x = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(x);
+
+// Task#06
+// var Email = "example@example.com";
+// alert("My email address is "+ Email);
+
+// Task#07
+//  var book = "Asmarter way to learn JavaScript";
+//  alert("I'm trying to learn from the book " +  book);
+
+// Task#08
+// document.write("Yah! I can write HTML content through javascript");
+
+// Task#09
+// var x = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(x);
+
+// Chapter 3 - Variables for Numbers
+
+// Task#01
+// var age = 22;
+// alert("I'm " + age + " years old")
+
+// Task#02
+// var n = 20;
+// alert("You have visited this site " + n +  " times");
+
+// Task#03
+// var birthYear = 1998;
+// document.write("My birth year is " +  birthYear +  "<br> Data type of my decleard variable is number");
+
+// Task#04
+// var VisitorName = "Jhone Doe";
+// var ProductTitle = " T_Shirt";
+// var quantity = 5;
+// document.write("<b>"+VisitorName + "</b> ordered <b>" + quantity + ProductTitle + "</b>(s) on XYZ Clothing store");
+
+// Chapter 4 - VARIABLE NAMES: LEGAL & ILLEGAL 
+
+// Task#01
+// var x,y,z;
+
+// Task#02 
+// legal variable
+// var a;
+// var _a;
+// var a_b;
+// var A1;
+// var A11_B;
+
+// illegal variables
+// var 1A;
+// var -a;
+// var @a;
+// var +b;
+// var a b;
+
+// Task#03
+document.write("<h3>Rules for naming JS variables</h3>");
+document.write("Variable names can only contain ______, ______,______ and ______.
+For example $my_1stVariable Variables must begin with a ______, ______ or
+_____. For example $name, _name or name
+ Variable names are case _________
+ Variable names should not be JS _________");
+
